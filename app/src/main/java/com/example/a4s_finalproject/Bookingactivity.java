e01c64ccff01912978c572e62df7684e9202e2ad
Sumandeep Kaur <sumandeep21111994@gmail.com>
2019-12-16 13:34:09 -0500

    Booking Activity

--- app/src/main/java/com/example/a4s_finalproject/Bookingactivity.java
+++ app/src/main/java/com/example/a4s_finalproject/Bookingactivity.java
@@ -3,12 +3,18 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Bookingactivity extends AppCompatActivity {
+
+    TextView txt_bookiid;
+    EditText editText_name, editText_contact, editText_itemname, editText_address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookingactivity);
+
     }
 }
