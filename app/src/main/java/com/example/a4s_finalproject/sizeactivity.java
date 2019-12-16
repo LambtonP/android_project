cb03d8843710b4a1736324aa60c961a886f99a13
Sumandeep Kaur <sumandeep21111994@gmail.com>
2019-12-16 13:39:36 -0500

    size activity

--- app/src/main/java/com/example/a4s_finalproject/sizeactivity.java
+++ app/src/main/java/com/example/a4s_finalproject/sizeactivity.java
@@ -3,8 +3,22 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class sizeactivity extends AppCompatActivity {
+
+    TextView txt_pizzas;
+    EditText editText_name,editText_mobile,editText_price,editText_address;
+    Spinner spinner_size;
+    ArrayAdapter adapter_size;
+    Button btn_Ok;
+    String TAG = "Android";
+    int priceofpizza = 0;
+    int totalvalue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
