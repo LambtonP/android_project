9a7f8adfd85a197aca7646844916f9e5c6e79aa6
Syed Nooruddin Fahad <Nooruddin93@gmail.com>
2019-12-16 09:58:56 -0400

    Login

--- app/src/main/java/com/example/a4s_finalproject/MainActivity.java
+++ app/src/main/java/com/example/a4s_finalproject/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText email , password ;
-    Button loginbutton , registartionbutton;
+    EditText email, password;
+    Button loginbutton, registartionbutton;
     TextView invisible;
 
     // getSupportActionBar().setTitle("Login")
@@ -20,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String text , text1;
+        String text, text1;
         email = findViewById(R.id.emailtext);
         password = findViewById(R.id.passtext);
         loginbutton = findViewById(R.id.login_button1);
         registartionbutton = findViewById(R.id.register);
         //invisible = findViewById(R.id.textView);
         Intent i = getIntent();
+        registartionbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, registration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
