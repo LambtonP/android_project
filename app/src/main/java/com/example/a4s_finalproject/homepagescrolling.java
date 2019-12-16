251ec65ebea74fa878dbf5cd3f5354c79296c316
maan-satveer <koursatveer12@gmail.com>
2019-12-16 12:05:17 -0400

    on create in scroll

--- app/src/main/java/com/example/a4s_finalproject/homepagescrolling.java
+++ app/src/main/java/com/example/a4s_finalproject/homepagescrolling.java
@@ -105,5 +105,11 @@ public class homepagescrolling extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_homepagescrolling, menu);
+        return true;
+    }
 
 }
