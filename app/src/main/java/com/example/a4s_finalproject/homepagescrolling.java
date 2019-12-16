830d7038e10c405a9b31a5259123e310182456c5
maan-satveer <koursatveer12@gmail.com>
2019-12-16 11:51:25 -0400

    submenu coding in homepage scrolling

--- app/src/main/java/com/example/a4s_finalproject/homepagescrolling.java
+++ app/src/main/java/com/example/a4s_finalproject/homepagescrolling.java
@@ -3,8 +3,14 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class homepagescrolling extends AppCompatActivity {
+
+    LinearLayout lyt_Pizza, lyt_calzones, lyt_pasta, lyt_wings, lyt_frechfries, lyt_soda;
+    TextView txt_frenchfry;
+    // SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
