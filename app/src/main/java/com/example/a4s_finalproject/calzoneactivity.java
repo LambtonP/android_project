ddc23866c1316daa053f4fe7e360a5a9dae810c2
Simranjeet Kaur <chakkalsimmi@gmail.com>
2019-12-16 14:06:05 -0500

    calzone activity

--- app/src/main/java/com/example/a4s_finalproject/calzoneactivity.java
+++ app/src/main/java/com/example/a4s_finalproject/calzoneactivity.java
@@ -3,8 +3,17 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class calzoneactivity extends AppCompatActivity {
+    GridView gridView;
+
+    int[] imageIcons = {R.drawable.pepperonicalzone, R.drawable.beconcalzone, R.drawable.greencalzone, R.drawable.mashrromcalzone,
+            R.drawable.pineapplecalzone, R.drawable.onioncalone, R.drawable.tomatocalzone};
+    String[] numbers = {
+            "Pepperoni", "Bacon", "Green pepper",
+            "Mashroom", "Pineapple", "Onion",
+            "Tomato & Hot Pepper"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
