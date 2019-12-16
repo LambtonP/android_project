17db78dca8ecb3d59a887dba9db4e28e8d9f0e26
Syed Nooruddin Fahad <Nooruddin93@gmail.com>
2019-12-16 09:53:50 -0400

    Assigning Values.

--- app/src/main/java/com/example/a4s_finalproject/MainActivity.java
+++ app/src/main/java/com/example/a4s_finalproject/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.a4s_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email , password ;
+    Button loginbutton , registartionbutton;
+    TextView invisible;
+
+    // getSupportActionBar().setTitle("Login")
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String text , text1;
+        email = findViewById(R.id.emailtext);
+        password = findViewById(R.id.passtext);
+        loginbutton = findViewById(R.id.login_button1);
+        registartionbutton = findViewById(R.id.register);
+        //invisible = findViewById(R.id.textView);
+        Intent i = getIntent();
     }
 }
