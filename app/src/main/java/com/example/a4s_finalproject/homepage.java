dd392e70db7b6821e88141ee379be1a558837b56
maan-satveer <koursatveer12@gmail.com>
2019-12-16 11:46:37 -0400

    assign values in homepage

--- app/src/main/java/com/example/a4s_finalproject/homepage.java
+++ app/src/main/java/com/example/a4s_finalproject/homepage.java
@@ -3,7 +3,9 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,5 +18,19 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        lyt_Pizza = findViewById(R.id.pizza);
+        lyt_calzones = findViewById(R.id.calzone);
+        lyt_pasta = findViewById(R.id.pasta);
+        lyt_wings = findViewById(R.id.chickenwings);
+        lyt_frechfries = findViewById(R.id.frechfries);
+        txt_frenchfry = findViewById(R.id.frenchfry);
+        lyt_soda = findViewById(R.id.soda);
+        lyt_Pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), pizzalist.class);
+                startActivity(intent);
+            }
+        });
     }
 }
