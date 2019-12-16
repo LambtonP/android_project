ccef5be078ba5b33e70f2617ca175c544d868ef6
Sumandeep Kaur <sumandeep21111994@gmail.com>
2019-12-16 13:51:03 -0500

    payment activity date.

--- app/src/main/java/com/example/a4s_finalproject/paymentactivity.java
+++ app/src/main/java/com/example/a4s_finalproject/paymentactivity.java
@@ -18,5 +18,22 @@ public class paymentactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paymentactivity);
+
+        edt_cardname=(EditText)findViewById(R.id.cardname);
+        edt_cardnumber=(EditText)findViewById(R.id.cardnumber);
+        edt_expdate=(EditText)findViewById(R.id.expdate);
+        edt_cvv=(EditText)findViewById(R.id.cvv);
+        btn_submit=(Button)findViewById(R.id.submit);
+        edt_cvv.getText().toString();
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+2, day);
+    }
+    private void showDate(int year, int i, int day) {
+        edt_expdate.setText(new StringBuilder().append(month).append("/").append(year));
+
+    }
     }
 }
