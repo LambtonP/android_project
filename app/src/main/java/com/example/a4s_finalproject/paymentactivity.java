290cbbd97387c6e282bc179f104d8b04ebbfde1d
Sumandeep Kaur <sumandeep21111994@gmail.com>
2019-12-16 13:49:53 -0500

    payment activity

--- app/src/main/java/com/example/a4s_finalproject/paymentactivity.java
+++ app/src/main/java/com/example/a4s_finalproject/paymentactivity.java
@@ -3,8 +3,16 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class paymentactivity extends AppCompatActivity {
+    EditText edt_cardname,edt_cardnumber,edt_expdate,edt_cvv;
+    Button btn_submit;
+    private int year,month, day;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
