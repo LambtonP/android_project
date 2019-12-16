b7ab774db5d4184230507113d23bb85304ee5787
Simranjeet Kaur <chakkalsimmi@gmail.com>
2019-12-16 15:00:42 -0500

    pasta activity inital code

--- app/src/main/java/com/example/a4s_finalproject/pastaactivity.java
+++ app/src/main/java/com/example/a4s_finalproject/pastaactivity.java
@@ -3,8 +3,13 @@ package com.example.a4s_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class pastaactivity extends AppCompatActivity {
+    GridView gridView;
+    int[] imageIcons = {R.drawable.pasta3, R.drawable.pasta1, R.drawable.pasta2, R.drawable.images};
+    String[] numbers = {
+            "Classic Tomato ", "Paprika Chicken", "White beans pasta", "Broccoli style pasta "};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
