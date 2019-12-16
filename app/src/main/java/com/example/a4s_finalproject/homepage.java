c000a14931b40a8c3830f9084bbf79cc31162292
maan-satveer <koursatveer12@gmail.com>
2019-12-16 11:45:43 -0400

    added views in homepage

--- app/src/main/java/com/example/a4s_finalproject/homepage.java
+++ app/src/main/java/com/example/a4s_finalproject/homepage.java
@@ -1,10 +1,16 @@
 package com.example.a4s_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class homepage extends AppCompatActivity {
+    CardView listoffood;
+    LinearLayout lyt_Pizza, lyt_calzones, lyt_pasta, lyt_wings, lyt_frechfries, lyt_soda;
+    TextView txt_frenchfry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
