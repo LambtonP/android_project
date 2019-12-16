4d63098dc8395087429c670d2376873c1926c1da
maan-satveer <koursatveer12@gmail.com>
2019-12-16 11:28:35 -0400

    Register & Login Intent

--- app/src/main/java/com/example/a4s_finalproject/MainActivity.java
+++ app/src/main/java/com/example/a4s_finalproject/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText email, password;
@@ -36,5 +37,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                String   user , passwordtext1;
+                user = email.getText().toString();
+                passwordtext1 = password.getText().toString();
+                //invisibletxt = invisible.getText().toString();
+
+                if(user.equals("chakkal@29") && passwordtext1.equals("simran")) {
+                    Toast.makeText(MainActivity.this, "welcome to homescreen " + user, Toast.LENGTH_SHORT).show();
+                    Intent i2 = new Intent(MainActivity.this,homepagescrolling.class);
+                    startActivity(i2);
+
+//                    Intent intent = new Intent(loginactivity.this, homepage.class);
+//                    startActivity(intent);
+                }
+                else if(user.equals("suman@12") && passwordtext1.equals("suman")) {
+
+                    Toast.makeText(MainActivity.this, "welcome to homescreen " + user, Toast.LENGTH_SHORT).show();
+                    Intent i2 = new Intent(MainActivity.this, homepagescrolling.class);
+                    startActivity(i2);
+                }
+
+                else if(user.equals("satveer@12") && passwordtext1.equals("satveer")) {
+                    Toast.makeText(MainActivity.this, "welcome to homescreen " + user, Toast.LENGTH_SHORT).show();
+                    Intent i2 = new Intent(MainActivity.this, homepagescrolling.class);
+                    startActivity(i2);
+                }
+//        }
+                else
+                    Toast.makeText(MainActivity.this,"login failed " + " choose correct email and password", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
